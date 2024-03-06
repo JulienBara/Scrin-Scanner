152933ab6505c5bf872d9e90037eb3bdfa60e99d
JulienBara <bara.julien@yahoo.fr>
2024-03-06 22:23:33 +0100

    scan wifi

---
+++ scan_wifi.py
@@ -0,0 +1,47 @@
+import time
+import requests
+import scapy.all as scapy
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+path = os.environ["BACKEND_URL"]
+
+def display_results(results):
+    print("Found {} devices.".format(len(results)))
+    print("IP Address\t\tMAC Address")
+    print("-----------------------------------------")
+    for result in results:
+        print(result["ip"] + "\t\t" + result["mac"])
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    results = []
+
+    for element in answered_list:
+        result = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        results.append(result)
+
+    display_results(results)
+    
+    return results
+
+target_ip = "192.168.1.1/24"
+
+while 1:
+    scan_results = scan(target_ip)    
+
+    for result in scan_results:
+        requests.post(
+            path+"/devices/heartbeat", 
+            data={
+                'address':result["mac"],
+                'name':result["ip"]
+            }
+        )
+
+    time.sleep(60)
