05e781f2ded1f6499939adcb1563770a1c27256f
JulienBara <bara.julien@yahoo.fr>
2024-04-06 19:38:37 +0200

    make dotenv load for explicit path

--- scan_wifi.py
+++ scan_wifi.py
@@ -2,11 +2,13 @@ import time
 import requests
 import scapy.all as scapy
 import os
+from os.path import join, dirname
 from dotenv import load_dotenv
 
 print("App is starting")
 
-load_dotenv()
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 path = os.environ["BACKEND_URL"]
 target_ip = os.environ["TARGET_IP"]
 
